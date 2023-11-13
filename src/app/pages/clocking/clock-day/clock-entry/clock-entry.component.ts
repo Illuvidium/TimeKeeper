345a97706a2cc242db2b8ac931a1b52a211a68b3
Rini <rini@lindhoud.com>
2023-11-13 18:38:07 +0100

    Use new angular template syntax

--- src/app/pages/clocking/clock-day/clock-entry/clock-entry.component.ts
+++ src/app/pages/clocking/clock-day/clock-entry/clock-entry.component.ts
@@ -1,72 +1,72 @@
 import {
-    ChangeDetectionStrategy,
-    ChangeDetectorRef,
-    Component,
-    Input,
-    OnDestroy,
-    OnInit,
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	Input,
+	OnDestroy,
+	OnInit,
 } from '@angular/core';
 import { ClockTime, Tag, Task } from '../../../../../../shared/entities';
 import {
-    calculateElapsedForDate,
-    getStartFinishTimeString,
+	calculateElapsedForDate,
+	getStartFinishTimeString,
 } from '../../../../shared/classes/clock-time-date-group';
 import { SubscriptionLike } from 'rxjs';
 import { ClockTimeService } from '../../../../shared/services/clock-time.service';
 
 @Component({
-    selector: 'app-clock-entry',
-    templateUrl: './clock-entry.component.html',
-    styleUrls: ['./clock-entry.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-clock-entry',
+	templateUrl: './clock-entry.component.html',
+	styleUrls: ['./clock-entry.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClockEntryComponent implements OnInit, OnDestroy {
-    @Input() date: Date | undefined;
-    @Input() tags: Tag[] = [];
-    @Input() tasks: Task[] = [];
-    @Input() clockTime: ClockTime | undefined;
+	@Input() date: Date | undefined;
+	@Input() tags: Tag[] = [];
+	@Input() tasks: Task[] = [];
+	@Input() clockTime: ClockTime | undefined;
 
-    private tickSubscription: SubscriptionLike;
-    protected taskDescription = '';
-    protected activeTags: Tag[] = [];
-    protected timeElapsedMs = 0;
-    protected startFinish = '';
+	private tickSubscription: SubscriptionLike;
+	protected taskDescription = '';
+	protected activeTags: Tag[] = [];
+	protected timeElapsedMs = 0;
+	protected startFinish = '';
 
-    constructor(
-        private clockTimeService: ClockTimeService,
-        private cdr: ChangeDetectorRef
-    ) {
-        this.tickSubscription = this.clockTimeService.tick.subscribe(() => {
-            if (!this.clockTime?.finish ?? false) {
-                this.setTimeData();
-                this.cdr.detectChanges();
-            }
-        });
-    }
+	constructor(
+		private clockTimeService: ClockTimeService,
+		private cdr: ChangeDetectorRef
+	) {
+		this.tickSubscription = this.clockTimeService.tick.subscribe(() => {
+			if (!this.clockTime?.finish ?? false) {
+				this.setTimeData();
+				this.cdr.detectChanges();
+			}
+		});
+	}
 
-    ngOnInit(): void {
-        const task = this.tasks.find((t) => t.id === this.clockTime?.task);
-        this.taskDescription = task?.name || '';
-        this.setTimeData();
+	ngOnInit(): void {
+		const task = this.tasks.find((t) => t.id === this.clockTime?.task);
+		this.taskDescription = task?.name || '';
+		this.setTimeData();
 
-        this.activeTags = this.tags.filter((t) =>
-            (task?.tags || []).includes(t.id)
-        );
-    }
+		this.activeTags = this.tags.filter((t) =>
+			(task?.tags || []).includes(t.id)
+		);
+	}
 
-    ngOnDestroy(): void {
-        this.tickSubscription.unsubscribe();
-    }
+	ngOnDestroy(): void {
+		this.tickSubscription.unsubscribe();
+	}
 
-    setTimeData(): void {
-        this.startFinish = getStartFinishTimeString(
-            this.clockTime as ClockTime,
-            this.date as Date
-        );
+	setTimeData(): void {
+		this.startFinish = getStartFinishTimeString(
+			this.clockTime as ClockTime,
+			this.date as Date
+		);
 
-        this.timeElapsedMs = calculateElapsedForDate(
-            this.clockTime as ClockTime,
-            this.date as Date
-        );
-    }
+		this.timeElapsedMs = calculateElapsedForDate(
+			this.clockTime as ClockTime,
+			this.date as Date
+		);
+	}
 }
