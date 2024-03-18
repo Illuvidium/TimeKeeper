b2e9052ba069df9d5657c6939c3f3ae3c7cb7d3b
Rini <rini@lindhoud.com>
2024-03-18 21:06:53 +0100

    Altered timestamp display

--- src/app/shared/components/time-stamp/time-stamp.component.ts
+++ src/app/shared/components/time-stamp/time-stamp.component.ts
@@ -1,43 +1,36 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-time-stamp',
-    templateUrl: './time-stamp.component.html',
-    styleUrls: ['./time-stamp.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-time-stamp',
+	templateUrl: './time-stamp.component.html',
+	styleUrls: ['./time-stamp.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TimeStampComponent {
-    @Input() set milliseconds(value: number) {
-        const millisecondsInASecond = 1000;
-        const millisecondsInAMinute = 60 * millisecondsInASecond;
-        const millisecondsInAnHour = 60 * millisecondsInAMinute;
-        const millisecondsInADay = 24 * millisecondsInAnHour;
+	@Input() set milliseconds(value: number) {
+		const millisecondsInASecond = 1000;
+		const millisecondsInAMinute = 60 * millisecondsInASecond;
+		const millisecondsInAnHour = 60 * millisecondsInAMinute;
 
-        this.days = Math.floor(value / millisecondsInADay);
-        value %= millisecondsInADay;
+		this.hours = Math.floor(value / millisecondsInAnHour);
+		value %= millisecondsInAnHour;
 
-        this.hours = Math.floor(value / millisecondsInAnHour);
-        value %= millisecondsInAnHour;
+		this.minutes = Math.floor(value / millisecondsInAMinute);
+		value %= millisecondsInAMinute;
 
-        this.minutes = Math.floor(value / millisecondsInAMinute);
-        value %= millisecondsInAMinute;
+		this.seconds = Math.floor(value / millisecondsInASecond);
 
-        this.seconds = Math.floor(value / millisecondsInASecond);
+		this.showHours = this.hours > 0;
+		this.showMinutes = this.showHours || this.minutes > 0;
+		this.showSeconds = !this.showMinutes;
+	}
 
-        this.showDays = this.days > 0;
-        this.showHours = this.showDays || this.hours > 0;
-        this.showMinutes = this.showHours || this.minutes > 0;
-        this.showSeconds = !this.showMinutes;
-    }
+	protected hours = 0;
+	protected minutes = 0;
+	protected seconds = 0;
+	protected showSeconds = true;
+	protected showMinutes = true;
+	protected showHours = true;
 
-    protected days = 0;
-    protected hours = 0;
-    protected minutes = 0;
-    protected seconds = 0;
-    protected showSeconds = true;
-    protected showMinutes = true;
-    protected showHours = true;
-    protected showDays = true;
-
-    constructor() {}
+	constructor() {}
 }
